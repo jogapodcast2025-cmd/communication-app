8609afef2847d755b42dcf696eeca2c107619b9e
Shex Barez <jogapodcast2025@gmail.com>
2026-09-23 00:28:33 +0300

    Initial Communication App

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+    console.log("A user connected");
+
+    socket.on("chat message", (data) => {
+        io.emit("chat message", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    });
+});
+
+server.listen(PORT, () => {
+    console.log("Communication App is running at http://localhost:" + PORT);
+});
